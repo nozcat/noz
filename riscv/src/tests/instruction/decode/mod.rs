65800add04006e94cac1309adf2d5864432ccaaa
nozcat <noz@noz.ai>
2025-07-07 09:23:18 -0600

    Move decode unsupported tests to separate module

--- riscv/src/tests/instruction/decode/mod.rs
+++ riscv/src/tests/instruction/decode/mod.rs
@@ -3,3 +3,4 @@ mod jump_instructions;
 mod load_instructions;
 mod register_instructions;
 mod system_instructions;
+mod unsupported;
--- riscv/src/tests/instruction/decode/system_instructions/ebreak.rs
+++ riscv/src/tests/instruction/decode/system_instructions/ebreak.rs
@@ -1,4 +1,3 @@
-
 use crate::instruction::RiscVInstruction;
 
 #[test]
